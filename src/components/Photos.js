4bb29fe26a8f1436e14b0b0a03e85282c55e1be8
Caolan Fanning <caolan.fanning@gmail.com>
2021-07-20 20:05:07 +0100

    about page

---
+++ src/components/Photos.js
@@ -0,0 +1,84 @@
+import React from "react";
+
+import { makeStyles } from "@material-ui/core/styles";
+import { useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+
+import Carousel from "react-material-ui-carousel";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles((theme) => ({
+	image: {
+		height: "300px",
+		width: "300px",
+		objectFit: "contain",
+	},
+	imageMobile: {
+		height: "200px",
+		width: "200px",
+		objectFit: "contain",
+	},
+	title: {
+		fontWeight: 600,
+	},
+}));
+
+const Photos = ({ images }) => {
+	const classes = useStyles();
+	const theme = useTheme();
+	const matches = useMediaQuery(theme.breakpoints.down("xs"));
+
+	return (
+		<Grid container>
+			{matches ? (
+				<Grid
+					item
+					container
+					justify='center'
+					align='center'
+					style={{ margin: "50px 0" }}>
+					<Grid item xs={12}>
+						<Carousel>
+							{images.map((image) => (
+								<>
+									<img
+										className={classes.image}
+										key={images.indexOf(image.img)}
+										src={image.img}
+									/>
+									<Typography className={classes.title} variant='subtitle1'>
+										{image.name}
+									</Typography>
+								</>
+							))}
+						</Carousel>
+					</Grid>
+				</Grid>
+			) : (
+				<Grid container style={{ margin: "50px 0" }}>
+					<Grid item container justify='center' align='center'>
+						{images.map((image) => (
+							<Grid item xs={12} md={6} lg={3}>
+								<Grid item xs={12}>
+									<img
+										className={classes.image}
+										src={image.img}
+										alt={`image #${images.indexOf(image)}`}
+									/>
+								</Grid>
+								<Grid item xs={12}>
+									<Typography className={classes.title} variant='subtitle1'>
+										{image.name}
+									</Typography>
+								</Grid>
+							</Grid>
+						))}
+					</Grid>
+				</Grid>
+			)}
+		</Grid>
+	);
+};
+
+export default Photos;
--- src/pages/Aboutpage.js
+++ src/pages/Aboutpage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Photos from "../components/Photos";
 
 import { makeStyles } from "@material-ui/core/styles";
 import { useTheme } from "@material-ui/core/styles";
@@ -6,9 +7,16 @@ import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
-import HeroSection from "../components/HeroSection";
-import Technologies from "../components/Technologies";
-import Projects from "../components/Projects";
+
+import brad from "../images/brad.png";
+import ania from "../images/ania.png";
+import deved from "../images/deved.png";
+import sonny from "../images/sonny.png";
+
+import youtube from "../images/youtube.png";
+import stack from "../images/stack.png";
+import udemy from "../images/udemy.png";
+import freecodecamp from "../images/freecodecamp.png";
 
 const useStyles = makeStyles((theme) => ({
 	container: {
@@ -16,14 +24,31 @@ const useStyles = makeStyles((theme) => ({
 		maxWidth: "1500px",
 		minWidth: "320px",
 	},
+	div: {
+		padding: "50px 0",
+	},
 }));
 
 const Aboutpage = () => {
 	const classes = useStyles();
+	const theme = useTheme();
+
+	const profilers = [
+		{ img: brad, name: "Brad Traversy" },
+		{ img: ania, name: "Ania Kubów" },
+		{ img: deved, name: "Dev Ed" },
+		{ img: sonny, name: "Sonny Sangha" },
+	];
+	const logos = [
+		{ img: youtube, name: "Youtube" },
+		{ img: stack, name: "StackOverFlow" },
+		{ img: freecodecamp, name: "FreeCodeCamp" },
+		{ img: udemy, name: "Udemy" },
+	];
 
 	return (
 		<Container className={classes.container}>
-			<div>
+			<div className={classes.div}>
 				<Typography variant='subtitle1'>
 					While obtaining my Mathematical Science Degree from T.U.D I attended a
 					computer programming module which based around the programming
@@ -46,14 +71,11 @@ const Aboutpage = () => {
 					every web application you visit in your day to day life.
 					<br /> These, of course, were the technologies HTML, CSS and
 					JavaScript!
-					<br />
-					<br />
+					<Photos images={logos} />
 					I found myself learning through a wide variety of resources online
 					which ranged from interactive learning websites and videos to reading
 					public forums. <br /> Though I found myself learning through many
 					different avenues, of course I had my favourite avenues of teaching...
-					<br />
-					<br />
 					I feel obligated to share my personal favourites as they have helped
 					me so much both on an individual basis and collectively in my pursuit
 					to become a professional software engineer! <br /> The websites that I
@@ -68,9 +90,10 @@ const Aboutpage = () => {
 					ungrateful of me not to mention these creators on an individual level
 					as they have helped me (and thousands of others) to reach a dream to
 					become a programmer.
-					<br /> <br />I have a great passion for learning and building
-					projects. I'm focused, driven and I would love an opportunity to learn
-					and grow not just as a developer but as a person.
+					<Photos images={profilers} />
+					I have a great passion for learning and building projects. I'm
+					focused, driven and I would love an opportunity to learn and grow not
+					just as a developer but as a person.
 					<br />
 					Thanks for reading!
 					<br /> Yours Sincerely, <br /> Caolan.
